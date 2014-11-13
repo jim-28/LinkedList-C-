f59609f28748ad99227252bc9e12e0d7eb8e0767
Jimmy Huang <jimmyhuang28@gmail.com>
2014-11-12 23:15:35 -0500

    Finished LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -8,31 +8,63 @@
 #define SWAHILIA "Karibu usio mpango kamba ya kuhifadhi"
 #define SWAHILIB "Tafadhali pembejeo neno moja"
 
-main(){
- 	
+struct node{
+	char x[50];
+	struct node *next;
+	};
+
+int main(){
+	/* INITIALIZE ROOT */
+	struct node *root;
+	struct node *conductor;
+	root=(struct node *) malloc(sizeof(struct node));
+	root->next=0;
+	root->x;
+	conductor=root;
+	int i;
+
 	char end[]="***END***";
 	char input[50];
 
-/* IF WE WANT THE PROGRAM IN SWAHILI, WE'LL REPLACE 'ENGLISH' WITH 'SWAHILI' */
+/* IF WE WANT THE PROGRAM IN SWAHILI, WE CAN REPLACE 'ENGLISH' WITH 'SWAHILI' */
 	printf("%s\n", ENGLISHA);
+	
 	while(1){
 	printf("%s:\n", ENGLISHB);
-	scanf(" %s", input);
+	scanf("%s", input);
 	
 	if(strcmp(input, end) == 0){
 /* PRINT LINKED LIST */
-		
-
+	conductor=root;
+	if (conductor!=0){
+    		while (conductor->next!=0) {
+        		printf("%s\n", conductor->x);
+        		conductor = conductor->next;
+   			}
+    		printf( "%s\n", conductor->x );
+		}		
 		return 0;
 		}
 	
 /* STORE INPUT IN LINKED LIST*/
-	
+	conductor->next=malloc(sizeof(struct node));
+	conductor=conductor->next;
 
-
+	if(conductor==0){
+		printf("Error: Out of memory");
+		return 0;
+		}
     /* INITIALIZE THE NEW NODE */
-    		
-	printf("Input: %s\n", input);
+	conductor->next = 0;
+	
+	for(i=0; i<sizeof(input);i++){
+		conductor->x[i] = input[i];    		
+	}
+	/* FREE UP INPUT FOR NEXT INPUT & CLEAR STDIN */
 	memset(&input[0], 0, sizeof(input));
+	fseek(stdin,0,SEEK_END);
+
+/* DEBUGGING */
+/* printf("INPUT:%s\n", conductor->x); */
 	}
 }
